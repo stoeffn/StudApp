6efc87b1c7c05b82c5035a1aec4bb644ccac1d47
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-10-31 17:24:19 +0100

    :tada: Add StudKit target

---
+++ StudApp/StudKit/StudKit.h
@@ -0,0 +1,19 @@
+//
+//  StudKit.h
+//  StudKit
+//
+//  Created by Steffen Ryll on 31.10.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StudKit.
+FOUNDATION_EXPORT double StudKitVersionNumber;
+
+//! Project version string for StudKit.
+FOUNDATION_EXPORT const unsigned char StudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StudKit/PublicHeader.h>
+
+
