2b22d6fa77db41488abb8bfc1f774be11a710783
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-10-31 19:56:31 +0100

    :lipstick: Add sign-up layout

---
+++ StudApp/StudApp/SignInControllerController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInControllerController.swift
+//  StudApp
+//
+//  Created by Steffen Ryll on 31.10.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import UIKit
+
+final class SignInControllerController: UITableViewController {
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
