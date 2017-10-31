a36cfa67c15b6fe9f7e8f4a332fd5a81c30990cd
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-10-31 17:20:53 +0100

    :tada: Add StudApp project

---
+++ StudApp/StudApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudApp
+//
+//  Created by Steffen Ryll on 31.10.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
