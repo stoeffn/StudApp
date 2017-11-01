98e4577af976c787f3864ce3f81e55163f601131
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-01 21:02:38 +0100

    :rotating_light: Add testing helpers

---
+++ StudApp/StudKitTests/Extensions/Data+Resource.swift
@@ -0,0 +1,18 @@
+//
+//  Data+Resource.swift
+//  StudKitTests
+//
+//  Created by Steffen Ryll on 28.07.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    init(fromResource filename: String) {
+        guard let url = Bundle(for: Setup.self).url(forResource: filename, withExtension: "json") else {
+            fatalError("Error getting file path for resource '\(filename)'.")
+        }
+        try! self.init(contentsOf: url)
+    }
+}
---
+++ StudApp/StudKitTests/Extensions/JSONDecoder+Resource.swift
@@ -0,0 +1,23 @@
+//
+//  JSONDecoder+Resource.swift
+//  StudKitTests
+//
+//  Created by Steffen Ryll on 25.07.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import Foundation
+
+extension JSONDecoder {
+    /// Decodes a top-level value of the given type from the given resourse file's contents that must be a JSON
+    /// representation.
+    ///
+    /// - parameter type: The type of the value to decode.
+    /// - parameter data: The data to decode from.
+    /// - returns: A value of the requested type.
+    /// - throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted, or if the given data is not valid JSON.
+    /// - throws: An error if any value throws an error during decoding.
+    public func decode<T>(_ type: T.Type, fromResource filename: String) throws -> T where T : Decodable {
+        return try decode(type, from: Data(fromResource: filename))
+    }
+}
