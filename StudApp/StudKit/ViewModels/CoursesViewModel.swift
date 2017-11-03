f34b8385e2f1c6372ddcdf6b5a491a983338d5af
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-03 12:30:38 +0100

    :sparkles: Add basic courses view model

---
+++ StudApp/StudKit/ViewModels/CoursesViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  CoursesViewModel.swift
+//  StudKit
+//
+//  Created by Steffen Ryll on 03.11.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import CoreData
+
+public final class CoursesViewModel : NSObject {
+    private let coreData = ServiceContainer.default[CoreDataService.self]
+    
+    private var courses = [Course]()
+    
+    weak public var delegate: DataSourceSectionDelegate?
+    
+    public override init() {
+        super.init()
+        controller.delegate = self
+    }
+    
+    private(set) lazy var controller: NSFetchedResultsController<Course>
+        = NSFetchedResultsController(fetchRequest: Course.fetchRequest(), managedObjectContext: coreData.viewContext,
+                                     sectionNameKeyPath: nil, cacheName: nil)
+}
+
+// MARK: - Data Source Section
+
+extension CoursesViewModel : DataSourceSection {
+    public typealias Row = Course
+    
+    public var numberOfRows: Int {
+        return courses.count
+    }
+    
+    public subscript(rowAt index: Int) -> Course {
+        return courses[index]
+    }
+}
+
+// MARK: - Fetched Results Controller Delegate
+
+extension CoursesViewModel : NSFetchedResultsControllerDelegate {
+    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.dataWillChange(in: self)
+    }
+    
+    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.dataDidChange(in: self)
+    }
+    
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any,
+                           at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let indexPath = newIndexPath else { return }
+            let course = self.controller.object(at: indexPath)
+            courses.insert(course, at: indexPath.row)
+            delegate?.data(changedIn: course, at: indexPath.row, change: .insert, in: self)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            let course = courses.remove(at: indexPath.row)
+            delegate?.data(changedIn: course, at: indexPath.row, change: .delete, in: self)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            let course = self.controller.object(at: indexPath)
+            courses[indexPath.row] = course
+            delegate?.data(changedIn: course, at: indexPath.row, change: .update(course), in: self)
+        case .move:
+            guard let indexPath = indexPath, let newIndexPath = newIndexPath else { return }
+            let course = courses.remove(at: indexPath.row)
+            courses.insert(course, at: newIndexPath.row)
+            delegate?.data(changedIn: course, at: indexPath.row, change: .move(to: newIndexPath.row), in: self)
+        }
+    }
+}
