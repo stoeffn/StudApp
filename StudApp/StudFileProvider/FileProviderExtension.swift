4bae0fbe53c4ef53aadaf1a7c23aed464b381770
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-11 19:39:46 +0100

    :construction: Add meta data methods to file provider extension

--- StudApp/StudFileProvider/FileProviderExtension.swift
+++ StudApp/StudFileProvider/FileProviderExtension.swift
@@ -6,140 +6,64 @@
 //  Copyright © 2017 Steffen Ryll. All rights reserved.
 //
 
+import CoreData
 import FileProvider
 import StudKit
 
 final class FileProviderExtension: NSFileProviderExtension {
-    var fileManager = FileManager()
+    // MARK: - Life Cycle
 
     override init() {
         ServiceContainer.default.register(providers: StudKitServiceProvider())
     }
 
-    func item(for _: NSFileProviderItemIdentifier) throws -> NSFileProviderItem? {
-        // resolve the given identifier to a record in the model
+    // MARK: - Providing Meta Data
 
-        // TODO: implement the actual lookup
-        return nil
+    override func persistentIdentifierForItem(at url: URL) -> NSFileProviderItemIdentifier? {
+        // Exploit the fact that the path structure has been defined as
+        // <base storage directory>/<item identifier>/<item file name>
+        guard url.pathComponents.count >= 2 else { return nil }
+        let id = url.pathComponents[url.pathComponents.count - 2]
+        return File.itemIdentifier(forId: id)
+    }
+
+    func containerUrlForItem(withPersistentIdentifier identifier: NSFileProviderItemIdentifier) -> URL {
+        return NSFileProviderManager.default.documentStorageURL
+            .appendingPathComponent(identifier.id, isDirectory: true)
     }
 
     override func urlForItem(withPersistentIdentifier identifier: NSFileProviderItemIdentifier) -> URL? {
-        // resolve the given identifier to a file on disk
-        guard let item = try? item(for: identifier) else {
-            return nil
-        }
-
-        // in this implementation, all paths are structured as <base storage directory>/<item identifier>/<item file name>
-        let manager = NSFileProviderManager.default
-        let perItemDirectory = manager.documentStorageURL.appendingPathComponent(identifier.rawValue, isDirectory: true)
-
-        return perItemDirectory.appendingPathComponent(item.filename, isDirectory: false)
+        guard let item = try? item(for: identifier) else { return nil }
+        return containerUrlForItem(withPersistentIdentifier: identifier)
+            .appendingPathComponent(item.filename, isDirectory: false)
     }
 
-    override func persistentIdentifierForItem(at url: URL) -> NSFileProviderItemIdentifier? {
-        // resolve the given URL to a persistent identifier using a database
-        let pathComponents = url.pathComponents
-
-        // exploit the fact that the path structure has been defined as
-        // <base storage directory>/<item identifier>/<item file name> above
-        assert(pathComponents.count > 2)
-
-        return NSFileProviderItemIdentifier(pathComponents[pathComponents.count - 2])
-    }
-
-    override func startProvidingItem(at _: URL, completionHandler: ((_ error: Error?) -> Void)?) {
-        // Should ensure that the actual file is in the position returned by URLForItemWithIdentifier:, then call the completion
-        // handler
-
-        /* TODO:
-         This is one of the main entry points of the file provider. We need to check whether the file already exists on disk,
-         whether we know of a more recent version of the file, and implement a policy for these cases. Pseudocode:
-
-         if !fileOnDisk {
-         downloadRemoteFile()
-         callCompletion(downloadErrorOrNil)
-         } else if fileIsCurrent {
-         callCompletion(nil)
-         } else {
-         if localFileHasChanges {
-         // in this case, a version of the file is on disk, but we know of a more recent version
-         // we need to implement a strategy to resolve this conflict
-         moveLocalFileAside()
-         scheduleUploadOfLocalFile()
-         downloadRemoteFile()
-         callCompletion(downloadErrorOrNil)
-         } else {
-         downloadRemoteFile()
-         callCompletion(downloadErrorOrNil)
-         }
-         }
-         */
-
-        completionHandler?(NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo: [:]))
-    }
-
-    override func itemChanged(at _: URL) {
-        // Called at some point after the file has changed; the provider may then trigger an upload
-
-        /* TODO:
-         - mark file at <url> as needing an update in the model
-         - if there are existing NSURLSessionTasks uploading this file, cancel them
-         - create a fresh background NSURLSessionTask and schedule it to upload the current modifications
-         - register the NSURLSessionTask with NSFileProviderManager to provide progress updates
-         */
-    }
-
-    override func stopProvidingItem(at url: URL) {
-        // Called after the last claim to the file has been released. At this point, it is safe for the file provider to remove
-        // the content file.
-        // Care should be taken that the corresponding placeholder file stays behind after the content file has been deleted.
-
-        // Called after the last claim to the file has been released. At this point, it is safe for the file provider to remove
-        // the content file.
-
-        // TODO: look up whether the file has local changes
-        let fileHasLocalChanges = false
-
-        if !fileHasLocalChanges {
-            // remove the existing file to free up space
-            do {
-                _ = try FileManager.default.removeItem(at: url)
-            } catch {
-                // Handle error
-            }
-
-            // write out a placeholder to facilitate future property lookups
-            providePlaceholder(at: url, completionHandler: { _ in
-                // TODO: handle any error, do any necessary cleanup
-            })
+    func model(for identifier: NSFileProviderItemIdentifier,
+               in context: NSManagedObjectContext) throws -> FileProviderItemConvertible? {
+        switch identifier.modelType {
+        case let .semester(id):
+            return try Semester.fetch(byId: id, in: context)
+        case let .course(id):
+            return try Course.fetch(byId: id, in: context)
+        case let .file(id):
+            return try File.fetch(byId: id, in: context)
+        case .root, .workingSet:
+            fatalError("Cannot fetch model of root container or working set.")
         }
     }
 
-    // MARK: - Actions
-
-    /* TODO: implement the actions for items here
-     each of the actions follows the same pattern:
-     - make a note of the change in the local model
-     - schedule a server request as a background task to inform the server of the change
-     - call the completion block with the modified item in its post-modification state
-     */
-
-    // MARK: - Enumeration
-
-    override func enumerator(for containerItemIdentifier: NSFileProviderItemIdentifier) throws -> NSFileProviderEnumerator {
-        let maybeEnumerator: NSFileProviderEnumerator? = nil
-        if containerItemIdentifier == NSFileProviderItemIdentifier.rootContainer {
-            // TODO: instantiate an enumerator for the container root
-        } else if containerItemIdentifier == NSFileProviderItemIdentifier.workingSet {
-            // TODO: instantiate an enumerator for the working set
-        } else {
-            // TODO: determine if the item is a directory or a file
-            // - for a directory, instantiate an enumerator of its subitems
-            // - for a file, instantiate an enumerator that observes changes to the file
+    override func item(for identifier: NSFileProviderItemIdentifier) throws -> NSFileProviderItem {
+        switch identifier.modelType {
+        case .root:
+            return try RootItem(context: coreData.viewContext)
+        case .workingSet:
+            throw "Not implemented: Working Set Item"
+        case let .semester(id):
+            return try SemesterItem(byId: id, context: coreData.viewContext)
+        case let .course(id):
+            return try CourseItem(byId: id, context: coreData.viewContext)
+        case let .file(id):
+            return try FileItem(byId: id, context: coreData.viewContext)
         }
-        guard let enumerator = maybeEnumerator else {
-            throw NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo: [:])
-        }
-        return enumerator
     }
 }
