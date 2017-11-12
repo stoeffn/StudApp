991ad710e4884593492d0226d62317c6cc0f4b01
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-12 11:00:28 +0100

    :sparkles: Add basic folder view model

---
+++ StudApp/StudKit/ViewModels/FolderViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  FolderViewModel.swift
+//  StudKit
+//
+//  Created by Steffen Ryll on 12.11.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import CoreData
+
+public final class FolderViewModel: NSObject {
+    private let coreDataService = ServiceContainer.default[CoreDataService.self]
+    private var fetchRequest: NSFetchRequest<File>
+
+    public weak var delegate: DataSourceSectionDelegate?
+
+    public init(fetchRequest: NSFetchRequest<File>) {
+        self.fetchRequest = fetchRequest
+        super.init()
+
+        controller.delegate = self
+    }
+
+    private(set) lazy var controller: NSFetchedResultsController<File>
+        = NSFetchedResultsController(fetchRequest: self.fetchRequest, managedObjectContext: coreDataService.viewContext,
+                                     sectionNameKeyPath: nil, cacheName: nil)
+
+    public func fetch() {
+        try? controller.performFetch()
+    }
+
+    public func update(handler: @escaping ResultHandler<Void>) {
+
+    }
+}
+
+// MARK: - Data Source Section
+
+extension FolderViewModel: DataSourceSection {
+    public typealias Row = File
+
+    public var numberOfRows: Int {
+        return controller.fetchedObjects?.count ?? 0
+    }
+
+    public subscript(rowAt index: Int) -> File {
+        return controller.object(at: IndexPath(row: index, section: 0))
+    }
+}
+
+// MARK: - Fetched Results Controller Delegate
+
+extension FolderViewModel: NSFetchedResultsControllerDelegate {
+    public func controllerWillChangeContent(_: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.dataWillChange(in: self)
+    }
+
+    public func controllerDidChangeContent(_: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.dataDidChange(in: self)
+    }
+
+    public func controller(_: NSFetchedResultsController<NSFetchRequestResult>, didChange object: Any,
+                           at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        guard let folder = object as? File else { fatalError() }
+        switch type {
+        case .insert:
+            guard let indexPath = newIndexPath else { return }
+            delegate?.data(changedIn: folder, at: indexPath.row, change: .insert, in: self)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            delegate?.data(changedIn: folder, at: indexPath.row, change: .delete, in: self)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            delegate?.data(changedIn: folder, at: indexPath.row, change: .update(folder), in: self)
+        case .move:
+            guard let indexPath = indexPath, let newIndexPath = newIndexPath else { return }
+            delegate?.data(changedIn: folder, at: indexPath.row, change: .move(to: newIndexPath.row), in: self)
+        }
+    }
+}
