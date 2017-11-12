3526319a1b6e3a44be8a7b3b010d80c5475581e6
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-12 19:38:47 +0100

    :bug: Fix signing out by overriding default credential

--- StudApp/StudKit/StudIp/StudIpService.swift
+++ StudApp/StudKit/StudIp/StudIpService.swift
@@ -22,12 +22,14 @@ final class StudIpService {
     /// - Warning: This does not garantuee that the credential is actually correct as this implementation only relies on a
     ///            credential being stored. Thus, the password might have changed in the meantime.
     var isSignedIn: Bool {
-        return URLCredentialStorage.shared.defaultCredential(for: api.protectionSpace) != nil
+        guard let credential = URLCredentialStorage.shared.defaultCredential(for: api.protectionSpace) else { return false }
+        return !(credential.user?.isEmpty ?? true)
     }
 
     /// Tries to sign into Stud.IP using the credentials provided.
     ///
     /// ## How it works
+    ///  0. Remove current default credential if set.
     ///  1. Create a new session credential with the provided username and password and save it as the default.
     ///  2. Request the password-protected discovery route, which provides information on available routes.
     ///  3. Remove session credential.
@@ -39,27 +41,37 @@ final class StudIpService {
     ///   - password: Stud.IP password.
     ///   - handler: Completion handler that is called after *every* step finished.
     func signIn(withUsername username: String, password: String, handler: @escaping ResultHandler<Void>) {
+        let protectionSpace = api.protectionSpace
+
+        if let credential = URLCredentialStorage.shared.defaultCredential(for: protectionSpace) {
+            URLCredentialStorage.shared.remove(credential, for: protectionSpace)
+        }
+
         let credential = URLCredential(user: username, password: password, persistence: .forSession)
-        URLCredentialStorage.shared.setDefaultCredential(credential, for: api.protectionSpace)
+        URLCredentialStorage.shared.setDefaultCredential(credential, for: protectionSpace)
 
         api.request(.discovery) { result in
-            URLCredentialStorage.shared.remove(credential, for: self.api.protectionSpace)
+            URLCredentialStorage.shared.remove(credential, for: protectionSpace)
 
             guard result.isSuccess else {
                 return handler(result.replacingValue(()))
             }
 
             let validatedCredential = URLCredential(user: username, password: password, persistence: .synchronizable)
-            URLCredentialStorage.shared.setDefaultCredential(validatedCredential, for: self.api.protectionSpace)
+            URLCredentialStorage.shared.setDefaultCredential(validatedCredential, for: protectionSpace)
 
             handler(result.replacingValue(()))
         }
     }
 
-    /// Removes the default credential used for authentication.
+    /// Removes the default credential used for authentication and replaces it with an empty credential.
     func signOut() {
         let protectionSpace = api.protectionSpace
+
         guard let credential = URLCredentialStorage.shared.defaultCredential(for: protectionSpace) else { return }
         URLCredentialStorage.shared.remove(credential, for: protectionSpace)
+
+        let emptyCredential = URLCredential(user: "", password: "", persistence: .synchronizable)
+        URLCredentialStorage.shared.setDefaultCredential(emptyCredential, for: protectionSpace)
     }
 }
