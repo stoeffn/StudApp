4b77db693722f3ac8022fab3fac4532f04d05ac3
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-13 07:33:35 +0100

    :memo: Document CDConvertible and NSManagedObjectContext utilities

--- StudApp/StudKit/Extensions/CoreData/CDConvertible.swift
+++ StudApp/StudKit/Extensions/CoreData/CDConvertible.swift
@@ -8,7 +8,13 @@
 
 import CoreData
 
+/// Something that can be converted to a core data managed object, e.g. a structure parsed from JSON.
 protocol CDConvertible {
+    /// Creates or updates a core data managed object using this object.
+    ///
+    /// - Remark: Implementations may fetch and update objects instead of creating them every time.
+    /// - Parameter context: Managed object context.
+    /// - Returns: Converted core data managed object.
     @discardableResult
     func coreDataModel(in context: NSManagedObjectContext) throws -> NSManagedObject
 }
--- StudApp/StudKit/Extensions/CoreData/NSMangedObjectContext+Utils.swift
+++ StudApp/StudKit/Extensions/CoreData/NSMangedObjectContext+Utils.swift
@@ -9,6 +9,12 @@
 import CoreData
 
 public extension NSManagedObjectContext {
+    /// Attempts to commit unsaved changes to registered objects to the context’s parent store if there are unsaved changes.
+    ///
+    /// If a context’s parent store is a persistent store coordinator, then changes are committed to the external store. If a
+    /// context’s parent store is another managed object context, then `save()` only updates managed objects in that parent
+    /// store. To commit changes to the external store, you must save changes in the chain of contexts up to and including the
+    /// context whose parent is the persistent store coordinator.
     func saveWhenChanged() throws {
         if hasChanges {
             try save()
