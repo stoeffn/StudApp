c7ee57bb317955c47d94519cb71e47b806211a02
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-14 19:52:20 +0100

    :art: Make API routes hashable

--- StudApp/StudKit/Api/ApiRoutes.swift
+++ StudApp/StudKit/Api/ApiRoutes.swift
@@ -6,12 +6,30 @@
 //  Copyright © 2017 Steffen Ryll. All rights reserved.
 //
 
-protocol ApiRoutes {
+protocol ApiRoutes: Hashable {
     var path: String { get }
 
     var type: Decodable.Type? { get }
 
     var method: HttpMethod { get }
+
+    var expiresAfter: TimeInterval? { get }
+}
+
+// MARK: - Equatable Conformance
+
+extension ApiRoutes {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.path == rhs.path
+    }
+}
+
+// MARK: - Hashable Conformance
+
+extension ApiRoutes {
+    public var hashValue: Int {
+        return path.hashValue
+    }
 }
 
 // MARK: - Default Implementation
@@ -23,5 +41,9 @@ extension ApiRoutes {
 
     var method: HttpMethod {
         return .get
+    }
+
+    var expiresAfter: TimeInterval? {
+        return nil
     }
 }
