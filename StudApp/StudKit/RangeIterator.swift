0690be307f2ebf5da6784e28d7ad8cbf7ea54348
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-16 07:32:39 +0100

    :memo: Document range iterator

--- StudApp/StudKit/RangeIterator.swift
+++ StudApp/StudKit/RangeIterator.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2017 Steffen Ryll. All rights reserved.
 //
 
+/// Given an element getter, this iterator iterates all integer values in `range`.
+///
+/// This class is useful when iterating over a data source that does not expose its underlying array.
 public struct RangeIterator<Element>: IteratorProtocol {
     private var currentIndex: Int
     private var range: CountableRange<Int>
     private var elementAt: (Int) -> Element
 
+    /// Creates a new range iterator.
+    ///
+    /// - Parameters:
+    ///   - range: Index range to iterate.
+    ///   - elementAt: Closure that gets an element at the index given.
     init(range: CountableRange<Int>, elementAt: @escaping (Int) -> Element) {
         currentIndex = range.lowerBound
         self.range = range
