96580a4cf5560118b6f92bfbf0306bae942a7308
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-16 19:39:19 +0100

    :bug: Fix ignoring expiry when requesting multiple pages of the same route

--- StudApp/StudKit/StudIp/Api+CollectionResponse.swift
+++ StudApp/StudKit/StudIp/Api+CollectionResponse.swift
@@ -11,27 +11,28 @@ private let defaultNumberOfItemsPerRequest = 20
 extension Api {
     @discardableResult
     func requestCollection<Result>(_ route: Routes, afterOffset offset: Int = 0,
-                                   itemsPerRequest: Int = defaultNumberOfItemsPerRequest,
+                                   itemsPerRequest: Int = defaultNumberOfItemsPerRequest, ignoreLastAccess: Bool = false,
                                    handler: @escaping ResultHandler<CollectionResponse<Result>>) -> URLSessionTask? {
         let offsetQuery = URLQueryItem(name: "offset", value: String(offset))
         let limitQuery = URLQueryItem(name: "limit", value: String(itemsPerRequest))
-        return requestDecoded(route, parameters: [offsetQuery, limitQuery]) { result in
+        return requestDecoded(route, parameters: [offsetQuery, limitQuery], ignoreLastAccess: ignoreLastAccess) { result in
             handler(result)
         }
     }
 
     func requestCompleteCollection<Value: Decodable>(_ route: Routes, afterOffset offset: Int = 0,
                                                      itemsPerRequest: Int = defaultNumberOfItemsPerRequest,
-                                                     items initialItems: [Value] = [],
+                                                     ignoreLastAccess: Bool = false, items initialItems: [Value] = [],
                                                      handler: @escaping ResultHandler<[Value]>) {
-        requestCollection(route, afterOffset: offset,
-                          itemsPerRequest: itemsPerRequest) { (result: Result<CollectionResponse<Value>>) in
+        requestCollection(route, afterOffset: offset, itemsPerRequest: itemsPerRequest,
+                          ignoreLastAccess: ignoreLastAccess) { (result: Result<CollectionResponse<Value>>) in
             guard let collection = result.value else {
                 return handler(result.replacingValue(nil))
             }
             let items = initialItems + collection.items
             if let offset = collection.pagination.nextOffset {
-                self.requestCompleteCollection(route, afterOffset: offset, items: items, handler: handler)
+                self.requestCompleteCollection(route, afterOffset: offset, ignoreLastAccess: true, items: items,
+                                               handler: handler)
             } else {
                 handler(result.replacingValue(items))
             }
