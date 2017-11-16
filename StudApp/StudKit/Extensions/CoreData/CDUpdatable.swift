5a1ca593d2d07b16d9783a0e9ec8661c388ad852
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-16 07:42:48 +0100

    :memo: Add basic documentation to CDUpdatable

--- StudApp/StudKit/Extensions/CoreData/CDUpdatable.swift
+++ StudApp/StudKit/Extensions/CoreData/CDUpdatable.swift
@@ -8,11 +8,18 @@
 
 import CoreData
 
+/// Something that can be updated in core data.
 protocol CDUpdatable {}
 
+// MARK: - Default Implementation
+
 extension CDUpdatable where Self: NSManagedObject {
+    /// Updates core data objects of this type using the result given. This includes inserting objects that are new in `result`
+    /// and updating properties of existing objects by overriding properties.
     static func update<Model: CDConvertible>(using result: Result<[Model]>, in context: NSManagedObjectContext,
                                              handler: @escaping ResultHandler<[Self]>) {
+        // TODO: Use a sequence instead of a result as input.
+        // TODO: Add ability to remove stale objects.
         guard let models = result.value else {
             return handler(result.replacingValue(nil))
         }
@@ -24,8 +31,11 @@ extension CDUpdatable where Self: NSManagedObject {
         }
     }
 
+    /// Updates or inserts one core data object of this type using the result given.
     static func update<Model: CDConvertible>(using result: Result<Model>, in context: NSManagedObjectContext,
                                              handler: @escaping ResultHandler<Self>) {
+        // TODO: Use an object instead of a result as input.
+        // TODO: Add ability to remove stale objects.
         guard let value = result.value else {
             return handler(result.replacingValue(nil))
         }
