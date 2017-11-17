4b99a39cac733adea5765515d7c451625b188743
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2017-11-17 07:46:36 +0100

    :memo: Document retrieving paginated collections from the API

--- StudApp/StudKit/StudIp/Api+CollectionResponse.swift
+++ StudApp/StudKit/StudIp/Api+CollectionResponse.swift
@@ -9,6 +9,19 @@
 private let defaultNumberOfItemsPerRequest = 20
 
 extension Api {
+    /// Requests data from this API and interprets it as a paginated collection.
+    ///
+    /// - Parameters:
+    ///   - route: Route to request data from.
+    ///   - offset: Pagination offset, i.e. the number of items to skip. Defaults to `0`.
+    ///   - itemsPerRequest: Number of items you want to receive from the API in one response. Defaults to
+    ///                      `defaultNumberOfItemsPerRequest`.
+    ///   - ignoreLastAccess: Whether to ignore the route's expiry policy. Defaults to `false`.
+    ///   - handler: Completion handler receiving a result with the decoded collection response containing a list of decoded
+    ///              data.
+    /// - Returns: URL task in its resumed state or `nil` if the route is not expired.
+    /// - Precondition: `route`'s type must not be `nil`.
+    /// - Remark: At the moment, this method supports JSON decoding only.
     @discardableResult
     func requestCollection<Result>(_ route: Routes, afterOffset offset: Int = 0,
                                    itemsPerRequest: Int = defaultNumberOfItemsPerRequest, ignoreLastAccess: Bool = false,
@@ -20,6 +33,19 @@ extension Api {
         }
     }
 
+    /// Requests all data from a paginated collection in this API by iterating every page.
+    ///
+    /// - Parameters:
+    ///   - route: Route to request data from.
+    ///   - offset: Pagination offset, i.e. the number of items to skip. Defaults to `0`.
+    ///   - itemsPerRequest: Number of items you want to receive from the API in one response. Defaults to
+    ///                      `defaultNumberOfItemsPerRequest`.
+    ///   - initialItems: Items that were already received from the API. As this method calls itself recursively, this argument
+    ///                   is used for appending data from previous requests. Usually, you should use its default empty value.
+    ///   - handler: Completion handler receiving a result with the decoded collection contents.
+    /// - Precondition: `route`'s type must not be `nil`.
+    /// - Remark: At the moment, this method supports JSON decoding only. Due to issueing multiple requests, this method does
+    ///           not return a single session task.
     func requestCompleteCollection<Value: Decodable>(_ route: Routes, afterOffset offset: Int = 0,
                                                      itemsPerRequest: Int = defaultNumberOfItemsPerRequest,
                                                      ignoreLastAccess: Bool = false, items initialItems: [Value] = [],
