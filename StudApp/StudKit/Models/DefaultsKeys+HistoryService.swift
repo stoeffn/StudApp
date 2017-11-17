8b1dcc3ca707e4dcda946a3affc377ba2fffb5eb
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-17 17:34:30 +0100

    :art: Add current history token extension to targets enumeration

--- StudApp/StudKit/Models/DefaultsKeys+HistoryService.swift
+++ StudApp/StudKit/Models/DefaultsKeys+HistoryService.swift
@@ -10,3 +10,15 @@ extension DefaultsKeys {
     static let currentAppHistoryToken = DefaultsKey<Data>(Targets.app.rawValue)
     static let currentFileProviderHistoryToken = DefaultsKey<Data>(Targets.fileProvider.rawValue)
 }
+
+// MARK: Targets Utilities
+
+extension Targets {
+    var currentHistoryTokenUserDefaultsKey: DefaultsKey<Data>? {
+        switch self {
+        case .app: return DefaultsKeys.currentAppHistoryToken
+        case .fileProvider: return DefaultsKeys.currentFileProviderHistoryToken
+        default: return nil
+        }
+    }
+}
