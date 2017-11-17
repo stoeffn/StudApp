661b1036a3791d95dca80bb0ff678841993a9cc5
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-17 17:51:13 +0100

    :sparkles: Add ability to encode and decode history tokens

--- StudApp/StudKit/Models/DefaultsKeys+HistoryService.swift
+++ StudApp/StudKit/Models/DefaultsKeys+HistoryService.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 Steffen Ryll. All rights reserved.
 //
 
+import CoreData
+
 extension DefaultsKeys {
     static let currentAppHistoryToken = DefaultsKey<Data>(Targets.app.rawValue)
     static let currentFileProviderHistoryToken = DefaultsKey<Data>(Targets.fileProvider.rawValue)
@@ -19,6 +21,25 @@ extension Targets {
         case .app: return DefaultsKeys.currentAppHistoryToken
         case .fileProvider: return DefaultsKeys.currentFileProviderHistoryToken
         default: return nil
+        }
+    }
+
+    var currentHistoryToken: NSPersistentHistoryToken? {
+        get {
+            guard let key = currentHistoryTokenUserDefaultsKey else { return nil }
+            let unarchiver = NSKeyedUnarchiver(forReadingWith: Defaults[key])
+            unarchiver.requiresSecureCoding = true
+            let token = unarchiver.decodeObject(of: NSPersistentHistoryToken.self, forKey: NSKeyedArchiveRootObjectKey)
+            unarchiver.finishDecoding()
+            return token
+        }
+        set {
+            guard let key = currentHistoryTokenUserDefaultsKey else { return }
+            let data = NSMutableData()
+            let archiver = NSKeyedArchiver(forWritingWith: data)
+            archiver.requiresSecureCoding = true
+            archiver.finishEncoding()
+            Defaults[key] = data as Data
         }
     }
 }
