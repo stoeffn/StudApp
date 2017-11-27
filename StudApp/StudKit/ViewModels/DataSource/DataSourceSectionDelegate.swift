709efbd9a4865603be3b668e463b72d898fca563
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-11-27 19:45:00 +0100

    :truck: Combine data source protocol and extensions into one file each

--- StudApp/StudKit/ViewModels/DataSource/DataSourceSectionDelegate.swift
+++ StudApp/StudKit/ViewModels/DataSource/DataSourceSectionDelegate.swift
@@ -25,3 +25,31 @@ public extension DataSourceSectionDelegate {
     func data<Section: DataSourceSection>(changedIn _: Section.Row, at _: Int, change _: DataChange<Section.Row, Int>,
                                           in _: Section) {}
 }
+
+// MARK: - Table View Controller Implementation
+
+public extension DataSourceSectionDelegate where Self: UITableViewController {
+    func dataWillChange<Section: DataSourceSection>(in _: Section) {
+        tableView.beginUpdates()
+    }
+
+    func dataDidChange<Section: DataSourceSection>(in _: Section) {
+        tableView.endUpdates()
+    }
+
+    func data<Section: DataSourceSection>(changedIn _: Section.Row, at index: Int,
+                                          change: DataChange<Section.Row, Int>, in _: Section) {
+        let indexPath = IndexPath(row: index, section: 0)
+        switch change {
+        case .insert:
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        case .delete:
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case let .move(newIndex):
+            let newIndexPath = IndexPath(row: newIndex, section: 0)
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+        }
+    }
+}
