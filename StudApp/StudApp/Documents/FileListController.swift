86e689b88db547bafe9d1e849fd2f1522a7b038a
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-09 19:14:38 +0100

    :bug: Only try to preview actual documents

--- StudApp/StudApp/Documents/FileListController.swift
+++ StudApp/StudApp/Documents/FileListController.swift
@@ -46,7 +46,10 @@ final class FileListController: UITableViewController, DataSourceSectionDelegate
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.cellForRow(at: indexPath) as? FileCell else { return }
+        guard
+            let cell = tableView.cellForRow(at: indexPath) as? FileCell,
+            !cell.file.isFolder
+        else { return }
 
         preview(cell.file)
         tableView.deselectRow(at: indexPath, animated: true)
