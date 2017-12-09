db8c9d6307437254b96e3881968111787b6f9de1
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-09 13:58:15 +0100

    :sparkles: Display course information in course view

--- StudApp/StudApp/Courses/CourseController.swift
+++ StudApp/StudApp/Courses/CourseController.swift
@@ -17,11 +17,43 @@ final class CourseController: UITableViewController, Routable {
         super.viewDidLoad()
 
         navigationItem.title = course.title
+
+        initUserInterface()
     }
 
     func prepareDependencies(for route: Routes) {
         guard case let .course(course) = route else { fatalError() }
 
         self.course = course
+    }
+
+    // MARK: - User Interface
+
+    @IBOutlet weak var subtitleLabel: UILabel!
+
+    @IBOutlet weak var courseNumberCell: UITableViewCell!
+
+    @IBOutlet weak var locationCell: UITableViewCell!
+
+    @IBOutlet weak var summaryCell: UITableViewCell!
+
+    @IBOutlet weak var summaryLabel: UILabel!
+
+    private func initUserInterface() {
+        subtitleLabel.text = course.subtitle
+
+        courseNumberCell.textLabel?.text = "Course Number".localized
+        courseNumberCell.detailTextLabel?.text = course.number
+
+        locationCell.textLabel?.text = "Location".localized
+        locationCell.detailTextLabel?.text = course.location
+
+        summaryLabel.text = course.summary
+    }
+
+    // MARK: - Table View Data Source
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
 }
