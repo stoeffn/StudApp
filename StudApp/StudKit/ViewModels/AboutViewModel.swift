96ad68ff23eb9d3a88a5bebb8648344f8e0bd6ce
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-09 21:34:43 +0100

    :pencil2: Remove "Icons" from icons8 description

--- StudApp/StudKit/ViewModels/AboutViewModel.swift
+++ StudApp/StudKit/ViewModels/AboutViewModel.swift
@@ -19,7 +19,7 @@ public final class AboutViewModel {
         (title: "Julian Lobe", description: "Beta-Tester, QA-Man, and Friend", url: nil),
         (title: "Cornelius Kater", description: "Support and Communication", url: URL(string: "http://ckater.de/")),
         (title: "Stud.IP e.V.", description: "Development of APIs", url: URL(string: "http://studip.de/")),
-        (title: "icons8", description: "Icons and Glyphs", url: URL(string: "https://icons8.com/")),
+        (title: "icons8", description: "Glyphs", url: URL(string: "https://icons8.com/")),
     ]
 }
 
