4feb89f35bf36a4f21edb5802c5bd7d5113ed8f2
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-11 22:27:12 +0100

    :building_construction: Use UIAlertController convenience initializers

--- StudApp/StudApp/Courses/CourseController.swift
+++ StudApp/StudApp/Courses/CourseController.swift
@@ -211,8 +211,7 @@ final class CourseController: UITableViewController, Routable {
             file.download { result in
                 guard result.isFailure else { return }
 
-                let alert = UIAlertController(title: result.error?.localizedDescription, message: nil, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Okay".localized, style: .default, handler: nil))
+                let alert = UIAlertController(title: result.error?.localizedDescription)
                 self.present(alert, animated: true, completion: nil)
             }
             return
