07fdda3dd3268e2d7e14823c028a4871c7bb74b6
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-12 18:43:24 +0100

    :sparkles: Add store outlets and actions

--- StudApp/StudKit/Controllers/Store/StoreController.swift
+++ StudApp/StudKit/Controllers/Store/StoreController.swift
@@ -11,7 +11,58 @@ import UIKit
 public final class StoreController: UITableViewController {
     // MARK: - Life Cycle
 
-    override public func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: - User Interface
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var subtitleLabel: UILabel!
+
+    @IBOutlet weak var trialButton: FilledButton!
+
+    @IBOutlet weak var orLabel: UILabel!
+
+    @IBOutlet weak var unlockButton: FilledButton!
+
+    @IBOutlet weak var restoreButton: UIButton!
+
+    @IBOutlet weak var disclaimerLabel: UILabel!
+
+    // MARK: - User Interaction
+
+    @IBAction
+    func moreButtonTapped(_ sender: Any) {
+        func showAboutView(_: UIAlertAction) {
+            performSegue(withRoute: .about)
+        }
+
+        func signOut(_: UIAlertAction) {
+            // TODO
+            print("TODO")
+        }
+
+        let barButtonItem = sender as? UIBarButtonItem
+
+        let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        controller.popoverPresentationController?.barButtonItem = barButtonItem
+        controller.addAction(UIAlertAction(title: "About".localized, style: .default, handler: showAboutView))
+        controller.addAction(UIAlertAction(title: "Sign Out".localized, style: .destructive, handler: signOut))
+        controller.addAction(UIAlertAction(title: "Cancel".localized, style: .cancel, handler: nil))
+        present(controller, animated: true, completion: nil)
+    }
+
+    @IBAction
+    func trialButtonTapped(_: Any) {
+    }
+
+    @IBAction
+    func unlockButtonTapped(_: Any) {
+    }
+
+    @IBAction
+    func restoreButtonTapped(_: Any) {
     }
 }
