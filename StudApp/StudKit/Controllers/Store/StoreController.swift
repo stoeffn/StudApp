a7f0aed2c702e2528cb014a728fdb3b76315154c
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-12 18:29:38 +0100

    :sparkles: Add store controller

---
+++ StudApp/StudKit/Controllers/Store/StoreController.swift
@@ -0,0 +1,17 @@
+//
+//  StoreController.swift
+//  StudKit
+//
+//  Created by Steffen Ryll on 12.12.17.
+//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//
+
+import UIKit
+
+public final class StoreController: UITableViewController {
+    // MARK: - Life Cycle
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
