116956f94994124055370e89c1768a609529edd9
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-24 14:01:02 +0100

    :globe_with_meridians: Localize settings view

--- StudApp/StudApp/Settings/SettingsController.swift
+++ StudApp/StudApp/Settings/SettingsController.swift
@@ -15,7 +15,45 @@ final class SettingsController: UITableViewController, Routable {
         super.viewDidLoad()
 
         navigationItem.title = "Settings".localized
+
+        downloadsCell.textLabel?.text = "Downloads".localized
+        removeDownloadsCell.textLabel?.text = "Remove All Downloads".localized
+
+        signOutCell.textLabel?.text = "Sign Out".localized
     }
+
+    // MARK: - Table View Data Source
+
+    private enum Sections: Int {
+        case documents, account
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch Sections(rawValue: section) {
+        case .documents?: return "Documents".localized
+        case .account?, nil: return nil
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch Sections(rawValue: section) {
+        case .documents?:
+            return [
+                "This accounts for the combined sizes of all documents that you have downloaded.".localized,
+                "They are not backed up by iTunes or iCloud.".localized
+            ].joined(separator: " ")
+        case .account?, nil:
+            return nil
+        }
+    }
+
+    // MARK: - User Interface
+
+    @IBOutlet weak var downloadsCell: UITableViewCell!
+
+    @IBOutlet weak var removeDownloadsCell: UITableViewCell!
+
+    @IBOutlet weak var signOutCell: UITableViewCell!
 
     // MARK: - User Interaction
 
