b0b3b9e07e2d582f1bb649a43e37da76a4fe46ab
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-27 00:58:44 +0100

    :lipstick: Implement event cell layout and labels

--- StudApp/StudApp/Events/EventCell.swift
+++ StudApp/StudApp/Events/EventCell.swift
@@ -13,13 +13,37 @@ final class EventCell: UITableViewCell {
 
     var event: Event! {
         didSet {
+            startsAtLabel.text = event.startsAt.formatted(using: .shortTime)
+            endsAtLabel.text = event.endsAt.formatted(using: .shortTime)
+
             colorView.backgroundColor = event.course.state.color
+
+            titleLabel.text = event.course.title
+
+            cancellationContainer.isHidden = !event.isCanceled
+            cancellationLabel.text = "Canceled".localized
+            cancellationReasonLabel.isHidden = event.cancellationReason == nil
+            cancellationReasonLabel.text = event.cancellationReason
+
+            locationLabel.isHidden = event.isCanceled || event.location == nil
+            locationLabel.text = event.location
         }
     }
 
     // MARK: - User Interface
 
+    @IBOutlet weak var startsAtLabel: UILabel!
+    @IBOutlet weak var endsAtLabel: UILabel!
+
     @IBOutlet weak var colorView: UIView!
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var cancellationContainer: UIStackView!
+    @IBOutlet weak var cancellationLabel: UILabel!
+    @IBOutlet weak var cancellationReasonLabel: UILabel!
+
+    @IBOutlet weak var locationLabel: UILabel!
 
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
         super.setHighlighted(highlighted, animated: animated)
