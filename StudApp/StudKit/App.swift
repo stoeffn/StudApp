5643965357f9aa2d525d76cb70a0c7a0db12a6f1
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-01-11 17:19:30 +0100

    :memo: Document App constants

--- StudApp/StudKit/App.swift
+++ StudApp/StudKit/App.swift
@@ -6,24 +6,35 @@
 //  Copyright © 2017 Steffen Ryll. All rights reserved.
 //
 
+/// Provides constants regarding app information.
 public enum App {
-    public static let id = "1317593772"
-
+    /// Display name of the application.
     public static let name = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String
 
+    /// Human-readable version name for this application.
     public static let versionName = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
 
+    /// The application's author.
     public static let authorName = "Steffen Ryll"
 
+    /// App Store URL.
     public static let url = URL(string: "https://itunes.apple.com/de/app/\(App.id)")
 
+    /// URL of the app's App Store review page.
     public static let reviewUrl = URL(string: "itms-apps://itunes.apple.com/de/app/\(App.id)?action=write-review")
 
+    /// Email address to send app feedback to.
     public static let feedbackMailAddress = "studapp@stoeffn.de"
 
+    /// Apple App Id as used by the App Store.
+    static let id = "1317593772"
+
+    /// Bundle of the `StudKit` framework.
     static let kitBundle = Bundle(for: StudKitServiceProvider.self)
 
+    /// Application group identifier used for sharing data between targets.
     static let groupIdentifier = "group.SteffenRyll.StudKit"
 
+    /// Application iCloud container identifier.
     static let iCloudContainerIdentifier = "iCloud.SteffenRyll.StudKit"
 }
