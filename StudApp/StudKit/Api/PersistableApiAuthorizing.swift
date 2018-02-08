96d8f5680a95a3d64cb0bfcfd32703731f16518d
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-08 19:13:09 +0100

    :sparkles: Add PersistableApiAuthorizing protocol

---
+++ StudApp/StudKit/Api/PersistableApiAuthorizing.swift
@@ -0,0 +1,17 @@
+//
+//  PersistableApiAuthorizing.swift
+//  StudKit
+//
+//  Created by Steffen Ryll on 08.02.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+protocol PersistableApiAuthorizing: ApiAuthorizing {
+    init?(fromPersistedService service: String)
+
+    var service: String { get }
+
+    func persistCredentials() throws
+
+    func removeCredentials() throws
+}
