26ec892d217eb21e58dc08c02d3b9daf214864a2
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-09 20:49:53 +0100

    :white_check_mark: Fix OAuth1 tests

--- StudApp/StudKitTests/Api/OAuth1Tests.swift
+++ StudApp/StudKitTests/Api/OAuth1Tests.swift
@@ -10,16 +10,26 @@ import XCTest
 @testable import StudKit
 
 final class OAuth1Tests: XCTestCase {
-    private let oAuth1 = OAuth1(api: MockApi<StudIpOAuth1Routes>(
-        baseUrl: URL(string: "https://www.example.com/")!), callbackUrl: App.signInCallbackUrl,
-    consumerKey: "dpf43f3p2l4k3l03", consumerSecret: "kd94hf93k423kf44")
+    private static let serviceName = "test"
+    private static let baseUrl = URL(string: "https://www.example.com/")!
+    private static let callbackUrl = URL(string: "callback://sign-in")!
+    private static let consumerKey = "dpf43f3p2l4k3l03"
+    private static let consumerSecret = "kd94hf93k423kf44"
+    private static let nonce = "323lvAsQ707p"
+    private static let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
+    private static let timestampString = "1191242096"
+    private static let signatureMethod = "HMAC-SHA1"
+    private static let version = "1.0"
+
+    private let oAuth1 = OAuth1(service: serviceName, api: MockApi<StudIpOAuth1Routes>(baseUrl: baseUrl),
+                                callbackUrl: callbackUrl, consumerKey: consumerKey, consumerSecret: consumerSecret)
 
     // MARK: - Coding
 
     func testDecodeParameter_Nonce() {
-        let parameter = oAuth1.decodeParameter(fromRawKeyAndValue: "oauth_nonce=1191242096")
+        let parameter = oAuth1.decodeParameter(fromRawKeyAndValue: "oauth_nonce=\(OAuth1Tests.nonce)")
         XCTAssertEqual(parameter?.0, .nonce)
-        XCTAssertEqual(parameter?.1, "1191242096")
+        XCTAssertEqual(parameter?.1, OAuth1Tests.nonce)
     }
 
     func testDecodeParameter_Token() {
@@ -61,75 +71,77 @@ final class OAuth1Tests: XCTestCase {
     }
 
     func testAuthorizationParametersForRequest() {
-        let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
         var request = URLRequest(url: URL(string: "https://www.example.com/dispatch.php/api/oauth/request_token")!)
         request.httpMethod = "POST"
-        let parameters = oAuth1.authorizationParameters(for: request, nonce: "kllo9940pd9333jh", timestamp: timestamp)
-        XCTAssertEqual(parameters, [
-            .callback: App.signInCallbackUrl.absoluteString,
-            .consumerKey: "dpf43f3p2l4k3l03",
-            .nonce: "kllo9940pd9333jh",
-            .signature: "5KgYgs7aqtCigdHY4ddU3VuSWD4%3D",
-            .signatureMethod: "HMAC-SHA1",
-            .timestamp: "1191242096",
-            .version: "1.0",
-        ])
+        let parameters = oAuth1.authorizationParameters(for: request, nonce: OAuth1Tests.nonce, timestamp: OAuth1Tests.timestamp)
+
+        XCTAssertEqual(parameters[.callback] ?? "", OAuth1Tests.callbackUrl.absoluteString)
+        XCTAssertEqual(parameters[.consumerKey] ?? "", OAuth1Tests.consumerKey)
+        XCTAssertEqual(parameters[.nonce] ?? "", OAuth1Tests.nonce)
+        XCTAssertEqual(parameters[.signature] ?? "", "wyx%2F7ZwCaexJOkeMsRX%2FV50bri4%3D")
+        XCTAssertEqual(parameters[.signatureMethod] ?? "", OAuth1Tests.signatureMethod)
+        XCTAssertEqual(parameters[.timestamp] ?? "", OAuth1Tests.timestampString)
+        XCTAssertEqual(parameters[.version] ?? "", OAuth1Tests.version)
     }
 
     func testAuthorizationHeaderForRequest() {
         var request = URLRequest(url: URL(string: "https://www.example.com/dispatch.php/api/oauth/request_token")!)
         request.httpMethod = "POST"
         let header = oAuth1.authorizationHeader(for: request)
-        XCTAssertTrue(header.starts(with: "OAuth oauth_consumer_key=\"dpf43f3p2l4k3l03\", oauth_nonce="), header)
+
+        XCTAssertTrue(header.starts(with: "OAuth oauth_callback="), header)
+        XCTAssertTrue(header.contains("oauth_signature="), header)
     }
 
     // MARK: - Signing Requests
 
     func testSignatureBaseForRequest_Post() {
-        let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
         var request = URLRequest(url: URL(string: "https://www.example.com/dispatch.php/api/oauth/request_token")!)
         request.httpMethod = "POST"
-        let base = oAuth1.signatureBase(for: request, nonce: "kllo9940pd9333jh", timestamp: timestamp)
+        let base = oAuth1.signatureBase(for: request, nonce: OAuth1Tests.nonce, timestamp: OAuth1Tests.timestamp)
+
         XCTAssertEqual(base, [
-            "POST&https%3A%2F%2Fwww.example.com%2Fdispatch.php%2Fapi%2Foauth%2Frequest_token&oauth_consumer_key",
-            "%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1",
-            "%26oauth_timestamp%3D1191242096%26oauth_version%3D1.0",
+            "POST&https%3A%2F%2Fwww.example.com%2Fdispatch.php%2Fapi%2Foauth%2Frequest_token",
+            "&oauth_callback%3Dcallback%253A%252F%252Fsign-in%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26",
+            "oauth_nonce%3D323lvAsQ707p%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp",
+            "%3D1191242096%26oauth_token%3D%26oauth_verifier%3D%26oauth_version%3D1.0",
         ].joined())
     }
 
     func testSignatureBaseForRequest_QueryParameters() {
-        let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
         var request = URLRequest(url: URL(string: "https://www.example.com/api/kittens?test=abc&something=cool!")!)
         request.httpMethod = "GET"
-        let base = oAuth1.signatureBase(for: request, nonce: "kllo9940pd9333jh", timestamp: timestamp)
+        let base = oAuth1.signatureBase(for: request, nonce: OAuth1Tests.nonce, timestamp: OAuth1Tests.timestamp)
+
         XCTAssertEqual(base, [
-            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce",
-            "%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_version",
-            "%3D1.0%26something%3Dcool%2521%26test%3Dabc",
+            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&oauth_callback%3Dcallback%253A%252F%252Fsign-in%26",
+            "oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3D323lvAsQ707p%26oauth_signature_method%3DHMAC-SHA1",
+            "%26oauth_timestamp%3D1191242096%26oauth_token%3D%26oauth_verifier%3D%26oauth_version%3D1.0%26something",
+            "%3Dcool%2521%26test%3Dabc",
         ].joined())
     }
 
     func testSignatureBaseForRequest_EmptyQueryParameters() {
-        let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
         var request = URLRequest(url: URL(string: "https://www.example.com/api/kittens?empty=")!)
         request.httpMethod = "GET"
-        let base = oAuth1.signatureBase(for: request, nonce: "kllo9940pd9333jh", timestamp: timestamp)
+        let base = oAuth1.signatureBase(for: request, nonce: OAuth1Tests.nonce, timestamp: OAuth1Tests.timestamp)
+
         XCTAssertEqual(base, [
-            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&empty%3D%26oauth_consumer_key",
-            "%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3D",
-            "HMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_version%3D1.0",
+            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&empty%3D%26oauth_callback%3Dcallback%253A%252F%252Fsign-in%26",
+            "oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3D323lvAsQ707p%26oauth_signature_method%3DHMAC-SHA1%26",
+            "oauth_timestamp%3D1191242096%26oauth_token%3D%26oauth_verifier%3D%26oauth_version%3D1.0",
         ].joined())
     }
 
     func testSignatureBaseForRequest_ArrayQueryParameters() {
         var request = URLRequest(url: URL(string: "https://www.example.com/api/kittens?test=abc&test=def")!)
         request.httpMethod = "GET"
-        let timestamp = Date(timeIntervalSince1970: 1_191_242_096)
-        let base = oAuth1.signatureBase(for: request, nonce: "kllo9940pd9333jh", timestamp: timestamp)
+        let base = oAuth1.signatureBase(for: request, nonce: OAuth1Tests.nonce, timestamp: OAuth1Tests.timestamp)
+
         XCTAssertEqual(base, [
-            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce",
-            "%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_version",
-            "%3D1.0%26test%3Dabc%26test%3Ddef",
+            "GET&https%3A%2F%2Fwww.example.com%2Fapi%2Fkittens&oauth_callback%3Dcallback%253A%252F%252Fsign-in%26",
+            "oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3D323lvAsQ707p%26oauth_signature_method%3DHMAC-SHA1%26",
+            "oauth_timestamp%3D1191242096%26oauth_token%3D%26oauth_verifier%3D%26oauth_version%3D1.0%26test%3Dabc%26test%3Ddef",
         ].joined())
     }
 
