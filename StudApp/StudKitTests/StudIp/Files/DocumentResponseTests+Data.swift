3eb1305312e4f6961024a0c91afc27fc9b14cebf
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-16 13:21:36 +0100

    :white_check_mark: Test converting document responses to core data objects

--- StudApp/StudKitTests/StudIp/Files/DocumentResponseTests+Data.swift
+++ StudApp/StudKitTests/StudIp/Files/DocumentResponseTests+Data.swift
@@ -9,12 +9,13 @@
 @testable import StudKit
 
 extension DocumentResponseTests {
-    static let documentData = """
+    static let document1Data = """
         {
             "id": "3b06880ecf63d7e3ac4f45b1947a09d9",
             "file_id": "3b06880ecf63d7e3ac4f45b1947a09d9",
             "folder_id": "e8471f3fde8c9c15a68e19e7691615d2",
             "downloads": "64",
+            "size": "1024",
             "description": "Sümmary",
             "content_terms_of_use_id": "0",
             "user_id": "4e89bbf43f0e31ecc0ca81e09d572e27",
@@ -27,4 +28,29 @@ extension DocumentResponseTests {
             "is_writable": false
         }
     """.data(using: .utf8)!
+
+    static let document2Data = """
+        {
+            "id": "4b06880ecf63d7e3ac4f45b1947a09d9",
+            "file_id": "4b06880ecf63d7e3ac4f45b1947a09d9",
+            "folder_id": "e8471f3fde8c9c15a68e19e7691615d2",
+            "description": "",
+            "content_terms_of_use_id": "0",
+            "user_id": "4e89bbf43f0e31ecc0ca81e09d572e27",
+            "name": "variante_B_04.png",
+            "mkdate": "1395426718",
+            "chdate": "1395426718",
+            "is_readable": true,
+            "is_downloadable": true,
+            "is_editable": false,
+            "is_writable": false
+        }
+    """.data(using: .utf8)!
+
+    static let document1 = DocumentResponse(id: "0", parentId: "1", userId: "2", name: "file.pdf",
+                                            createdAt: Date(timeIntervalSince1970: 10), modifiedAt: Date(timeIntervalSince1970: 20),
+                                            summary: "Sümmary", size: 1024, downloadCount: 42)
+
+    static let document2 = DocumentResponse(id: "8", parentId: "7", name: "image.png", createdAt: Date(timeIntervalSince1970: 1),
+                                            modifiedAt: Date(timeIntervalSince1970: 2))
 }
--- StudApp/StudKitTests/StudIp/Files/DocumentResponseTests.swift
+++ StudApp/StudKitTests/StudIp/Files/DocumentResponseTests.swift
@@ -6,15 +6,30 @@
 //  Copyright © 2018 Steffen Ryll. All rights reserved.
 //
 
+import CoreData
 import MobileCoreServices
 import XCTest
 @testable import StudKit
 
 final class DocumentResponseTests: XCTestCase {
     let decoder = ServiceContainer.default[JSONDecoder.self]
+    var context: NSManagedObjectContext!
 
-    func testInit_DocumentData_Document() {
-        let document = try! decoder.decode(DocumentResponse.self, from: DocumentResponseTests.documentData)
+    // MARK: - Life Cycle
+
+    override func setUp() {
+        context = StudKitTestsServiceProvider(currentTarget: .tests).provideCoreDataService().viewContext
+
+        _ = try! FolderResponse(id: "1", courseId: "0").coreDataModel(in: context)
+        _ = try! FolderResponse(id: "7", courseId: "0").coreDataModel(in: context)
+
+        _ = try! UserResponse(id: "2").coreDataModel(in: context)
+    }
+
+    // MARK: - Coding
+
+    func testInit_Document1Data() {
+        let document = try! decoder.decode(DocumentResponse.self, from: DocumentResponseTests.document1Data)
         XCTAssertEqual(document.id, "3b06880ecf63d7e3ac4f45b1947a09d9")
         XCTAssertEqual(document.parentId, "e8471f3fde8c9c15a68e19e7691615d2")
         XCTAssertEqual(document.userId, "4e89bbf43f0e31ecc0ca81e09d572e27")
@@ -23,7 +38,51 @@ final class DocumentResponseTests: XCTestCase {
         XCTAssertEqual(document.modifiedAt.debugDescription, "2014-03-21 18:31:58 +0000")
         XCTAssertEqual(document.summary, "Sümmary")
         XCTAssertEqual(document.downloadCount, 64)
-        XCTAssertEqual(document.size, nil)
+        XCTAssertEqual(document.size, 1024)
         XCTAssertEqual(document.typeIdentifier, kUTTypePNG as String)
+    }
+
+    func testInit_Document2Data() {
+        let document = try! decoder.decode(DocumentResponse.self, from: DocumentResponseTests.document2Data)
+        XCTAssertEqual(document.id, "4b06880ecf63d7e3ac4f45b1947a09d9")
+        XCTAssertEqual(document.parentId, "e8471f3fde8c9c15a68e19e7691615d2")
+        XCTAssertEqual(document.userId, "4e89bbf43f0e31ecc0ca81e09d572e27")
+        XCTAssertEqual(document.name, "variante_B_04.png")
+        XCTAssertEqual(document.createdAt.debugDescription, "2014-03-21 18:31:58 +0000")
+        XCTAssertEqual(document.modifiedAt.debugDescription, "2014-03-21 18:31:58 +0000")
+        XCTAssertNil(document.summary)
+        XCTAssertNil(document.downloadCount)
+        XCTAssertNil(document.size)
+        XCTAssertEqual(document.typeIdentifier, kUTTypePNG as String)
+    }
+
+    // MARK: - Converting to Core Data Objects
+
+    func testCoreDataObject_Document1() {
+        let file = try! DocumentResponseTests.document1.coreDataModel(in: context) as! File
+        XCTAssertEqual(file.id, "0")
+        XCTAssertEqual(file.name, "file.pdf")
+        XCTAssertEqual(file.typeIdentifier, kUTTypePDF as String)
+        XCTAssertEqual(file.size, 1024)
+        XCTAssertEqual(file.parent?.id, "1")
+        XCTAssertEqual(file.createdAt, Date(timeIntervalSince1970: 10))
+        XCTAssertEqual(file.modifiedAt, Date(timeIntervalSince1970: 20))
+        XCTAssertEqual(file.downloadCount, 42)
+        XCTAssertEqual(file.summary, "Sümmary")
+        XCTAssertEqual(file.owner?.id, "2")
+    }
+
+    func testCoreDataObject_Document2() {
+        let file = try! DocumentResponseTests.document2.coreDataModel(in: context) as! File
+        XCTAssertEqual(file.id, "8")
+        XCTAssertEqual(file.name, "image.png")
+        XCTAssertEqual(file.typeIdentifier, kUTTypePNG as String)
+        XCTAssertEqual(file.size, -1)
+        XCTAssertEqual(file.parent?.id, "7")
+        XCTAssertEqual(file.createdAt, Date(timeIntervalSince1970: 1))
+        XCTAssertEqual(file.modifiedAt, Date(timeIntervalSince1970: 2))
+        XCTAssertEqual(file.downloadCount, -1)
+        XCTAssertNil(file.summary)
+        XCTAssertNil(file.owner)
     }
 }
