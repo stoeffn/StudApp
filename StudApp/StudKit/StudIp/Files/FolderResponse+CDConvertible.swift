73f607dd76c33f0d26a2f3ff6f1b90bb03d22be4
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-16 12:48:40 +0100

    :sparkles: Make document and folder response convertible to Core Data objects

---
+++ StudApp/StudKit/StudIp/Files/FolderResponse+CDConvertible.swift
@@ -0,0 +1,32 @@
+//
+//  FolderResponse+CDConvertible.swift
+//  StudKit
+//
+//  Created by Steffen Ryll on 16.02.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+import CoreData
+import MobileCoreServices
+
+extension FolderResponse: CDConvertible {
+    func coreDataModel(in context: NSManagedObjectContext) throws -> NSManagedObject {
+        let (file, _) = try File.fetch(byId: id, orCreateIn: context)
+        let folders = try self.folders
+            .flatMap { try $0.coreDataModel(in: context) as? File }
+        let documents = try self.documents
+            .flatMap { try $0.coreDataModel(in: context) as? File }
+        file.id = id
+        file.typeIdentifier = kUTTypeFolder as String
+        file.parent = try File.fetch(byId: parentId, in: context)
+        file.course = try Course.fetch(byId: courseId, in: context) ?? file.course
+        file.owner = try User.fetch(byId: userId, in: context)
+        file.name = name
+        file.createdAt = createdAt
+        file.modifiedAt = modifiedAt
+        file.summary = summary
+        file.children = Set(folders).union(documents)
+        file.children.forEach { $0.course = file.course }
+        return file
+    }
+}
