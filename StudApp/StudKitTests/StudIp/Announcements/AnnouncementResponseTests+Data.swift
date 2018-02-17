aa76be3098fe582b84bc844f374a156d5f060cca
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-17 14:57:54 +0100

    :white_check_mark: Update announcement response tests

--- StudApp/StudKitTests/StudIp/Announcements/AnnouncementResponseTests+Data.swift
+++ StudApp/StudKitTests/StudIp/Announcements/AnnouncementResponseTests+Data.swift
@@ -9,24 +9,51 @@
 @testable import StudKit
 
 extension AnnouncementResponseTests {
-    static let announcementData = """
+    static let announcement0Data = """
         {
-            "news_id": "d8b6b4df0950483bc1911adfff93737b",
+            "news_id": "A0",
             "topic": "Weihnachtspause",
             "body": "Liebe Studierende,\\r\\n\\r\\ndies ist ein Test.",
             "date": "1513033200",
-            "user_id": "1161ca9496cea513f9abd7ded50aac40",
+            "user_id": "U0",
             "expire": "2419140",
             "allow_comments": "1",
             "chdate": "1513079322",
             "chdate_uid": "",
             "mkdate": "1513079322",
             "body_html": "<div class=\\"formatted-content\\">Liebe Studierende,<br><br>dies ist ein Test.<\\/div>",
-            "comments": "\\/api.php\\/news\\/d8b6b4df0950483bc1911adfff93737b\\/comments",
+            "comments": "\\/api.php\\/news\\/A0\\/comments",
             "comments_count": 0,
             "ranges": [
-                "\\/api.php\\/course\\/c9d9012189da913f029a1ab9a2781a98\\/news"
+                "\\/api.php\\/course\\/C0\\/news"
             ]
         }
     """.data(using: .utf8)!
+
+    static let announcement1Data = """
+        {
+            "news_id": "A1",
+            "topic": "Title",
+            "body": "Another test.",
+            "date": "1513033200",
+            "user_id": "",
+            "expire": "2419140",
+            "allow_comments": "0",
+            "chdate": "1513079322",
+            "chdate_uid": "",
+            "mkdate": "1513079322",
+            "body_html": "<div class=\\"formatted-content\\">Liebe Studierende,<br><br>dies ist ein Test.<\\/div>",
+            "comments": "\\/api.php\\/news\\/A0\\/comments",
+            "comments_count": 0,
+            "ranges": [
+                "\\/api.php\\/course\\/C0\\/news",
+                "\\/api.php\\/course\\/C1\\/news"
+            ]
+        }
+    """.data(using: .utf8)!
+
+    static let announcement0 = AnnouncementResponse(id: "A0", courseIds: ["C0", "C1"], userId: "U0",
+                                                    createdAt: Date(timeIntervalSince1970: 1),
+                                                    modifiedAt: Date(timeIntervalSince1970: 2),
+                                                    expiresAfter: 3, title: "Title", body: "Body")
 }
--- StudApp/StudKitTests/StudIp/Announcements/AnnouncementResponseTests.swift
+++ StudApp/StudKitTests/StudIp/Announcements/AnnouncementResponseTests.swift
@@ -12,14 +12,61 @@ import XCTest
 
 final class AnnouncementResponseTests: XCTestCase {
     let decoder = ServiceContainer.default[JSONDecoder.self]
+    var context: NSManagedObjectContext!
 
-    func testInit_AnnouncmentData_Annoucnment() {
-        let announcement = try! decoder.decode(AnnouncementResponse.self, from: AnnouncementResponseTests.announcementData)
-        XCTAssertEqual(announcement.id, "d8b6b4df0950483bc1911adfff93737b")
-        XCTAssertEqual(announcement.createdAt!.debugDescription, "2017-12-12 11:48:42 +0000")
-        XCTAssertEqual(announcement.modifiedAt!.debugDescription, "2017-12-12 11:48:42 +0000")
-        XCTAssertEqual(announcement.expiresAt!.debugDescription, "2018-01-09 11:47:42 +0000")
+    // MARK: - Life Cycle
+
+    override func setUp() {
+        context = StudKitTestsServiceProvider(currentTarget: .tests).provideCoreDataService().viewContext
+
+        try! CourseResponse(id: "C0").coreDataObject(in: context)
+        try! CourseResponse(id: "C1").coreDataObject(in: context)
+
+        try! UserResponse(id: "U0").coreDataObject(in: context)
+    }
+
+    // MARK: - Coding
+
+    func testInit_Announcement0() {
+        let announcement = try! decoder.decode(AnnouncementResponse.self, from: AnnouncementResponseTests.announcement0Data)
+        XCTAssertEqual(announcement.id, "A0")
+        XCTAssertEqual(announcement.courseIds, ["C0"])
+        XCTAssertEqual(announcement.userId, "U0")
+        XCTAssertEqual(announcement.createdAt.debugDescription, "2017-12-12 11:48:42 +0000")
+        XCTAssertEqual(announcement.modifiedAt.debugDescription, "2017-12-12 11:48:42 +0000")
+        XCTAssertEqual(announcement.expiresAt.debugDescription, "2018-01-09 11:47:42 +0000")
         XCTAssertEqual(announcement.title, "Weihnachtspause")
         XCTAssertEqual(announcement.body, "Liebe Studierende,\r\n\r\ndies ist ein Test.")
+    }
+
+    func testInit_Announcement1() {
+        let announcement = try! decoder.decode(AnnouncementResponse.self, from: AnnouncementResponseTests.announcement1Data)
+        XCTAssertEqual(announcement.id, "A1")
+        XCTAssertEqual(announcement.courseIds, ["C0", "C1"])
+        XCTAssertEqual(announcement.userId, nil)
+        XCTAssertEqual(announcement.createdAt.debugDescription, "2017-12-12 11:48:42 +0000")
+        XCTAssertEqual(announcement.modifiedAt.debugDescription, "2017-12-12 11:48:42 +0000")
+        XCTAssertEqual(announcement.expiresAt.debugDescription, "2018-01-09 11:47:42 +0000")
+        XCTAssertEqual(announcement.title, "Title")
+        XCTAssertEqual(announcement.body, "Another test.")
+    }
+
+    func testInit_AnnouncementCollection() {
+        let collection = try! decoder.decode(CollectionResponse<AnnouncementResponse>.self, fromResource: "announcementCollection")
+        XCTAssertEqual(collection.items.count, 2)
+    }
+
+    // MARK: - Converting to a Core Data Object
+
+    func testCoreDataObject() {
+        let announcement = try! AnnouncementResponseTests.announcement0.coreDataObject(in: context)
+        XCTAssertEqual(announcement.id, "A0")
+        XCTAssertEqual(Set(announcement.courses.map { $0.id }), ["C0", "C1"])
+        XCTAssertEqual(announcement.user?.id, "U0")
+        XCTAssertEqual(announcement.createdAt, Date(timeIntervalSince1970: 1))
+        XCTAssertEqual(announcement.modifiedAt, Date(timeIntervalSince1970: 2))
+        XCTAssertEqual(announcement.expiresAt, Date(timeIntervalSince1970: 4))
+        XCTAssertEqual(announcement.title, "Title")
+        XCTAssertEqual(announcement.body, "Body")
     }
 }
