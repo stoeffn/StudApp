28e51f3c7e77cbed3bf4f478e3e43a3f3b3b298e
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-22 14:41:53 +0100

    :clown_face: Add some mock data

--- StudApp/StudKit/StudIp/MockResponses.swift
+++ StudApp/StudKit/StudIp/MockResponses.swift
@@ -18,18 +18,33 @@ struct MockResponses {
                          endsAt: Date(timeIntervalSince1970: 1519924860)),
     ]
 
-    lazy var currentUser = UserResponse(id: "U0", username: "student", givenName: "Successful", familyName: "Student")
+    lazy var currentUser = UserResponse(id: "U0", username: "murphy", givenName: "Murphy", familyName: "Cooper")
 
     lazy var theCount = UserResponse(id: "U1", username: "theCount".localized, givenName: "Count".localized,
                                      familyName: "von Count".localized)
 
+    lazy var professorProton = UserResponse(id: "U2", username: "proton", givenName: "Professor", familyName: "Proton")
+
+    lazy var langdon = UserResponse(id: "U3", username: "langdon", givenName: "Robert", familyName: "Langdon")
+
+    lazy var tesla = UserResponse(id: "U4", username: "tesla", givenName: "Nikola", familyName: "Tesla")
+
+    lazy var cooper = UserResponse(id: "U5", username: "coop", givenName: "Joseph", familyName: "Cooper")
+
     lazy var courses = [
-        CourseResponse(id: "C0", number: "3.00000000001", title: "Numerical Analysis".localized,
+        CourseResponse(id: "C0", number: "1.00000000001", title: "Numerical Analysis".localized,
                        subtitle: "Optimizing Algorithms for Computers".localized, location: "Bielefeld Room".localized,
-                       summary: nil, lecturers: [theCount], beginSemesterId: "S2", endSemesterId: "S2"),
-        CourseResponse(id: "C1", number: "42", title: "Computer Architecture".localized, subtitle: nil,
-                       location: "Multimedia Room".localized,
-                       summary: "In this lecture, you will learn how to…", lecturers: [], beginSemesterId: "S2", endSemesterId: "S2"),
+                       groupId: 1, lecturers: [theCount], beginSemesterId: "S2", endSemesterId: "S2"),
+        CourseResponse(id: "C1", number: "3.14", title: "Linear Algebra I".localized,
+                       subtitle: "Vectors and Stuff".localized, location: "Hugo Kulka Room".localized,
+                       groupId: 1, lecturers: [cooper, theCount], beginSemesterId: "S2", endSemesterId: "S2"),
+        CourseResponse(id: "C2", number: "42", title: "Computer Architecture".localized,
+                       location: "Multimedia Room".localized, summary: "In this lecture, you will learn how to…",
+                       groupId: 3, lecturers: [professorProton], beginSemesterId: "S2", endSemesterId: "S2"),
+        CourseResponse(id: "C3", number: nil, title: "Theoretical Stud.IP Science".localized,
+                       groupId: 4, lecturers: [professorProton], beginSemesterId: "S2", endSemesterId: "S2"),
+        CourseResponse(id: "C3", number: nil, title: "Data Science 101".localized,
+                       groupId: 4, lecturers: [tesla], beginSemesterId: "S2", endSemesterId: "S2"),
     ]
 
     mutating func insert(into context: NSManagedObjectContext) throws {
