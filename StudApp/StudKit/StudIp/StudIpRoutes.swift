82cf7f926890d95b2936d1e0d9362a799f1a1295
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-25 19:30:18 +0100

    :sparkles: Add discovery Stud.IP route and sort routes alphabetically

--- StudApp/StudKit/StudIp/StudIpRoutes.swift
+++ StudApp/StudKit/StudIp/StudIpRoutes.swift
@@ -8,24 +8,23 @@
 
 /// Selection of API routes exposed by the Stud.IP API.
 public enum StudIpRoutes: ApiRoutes {
-    /// Returns all semesters, usually starting about ten years in the past and ending one year in the
-    /// future.
-    case semesters
+    /// Returns a collection of unexpired announcements for the course with the id given.
+    case announcementsInCourse(withId: String)
+
+    /// Returns the current user.
+    case currentUser
 
     /// Returns all courses that a user with the given id is enrolled in.
     case courses(forUserId: String)
 
-    /// Returns the root folder along with its first-level children.
-    case rootFolderForCourse(withId: String)
-
-    /// Returns a folder along with its first-level children.
-    case folder(withId: String)
+    /// Returns information on what routes are available.
+    case discovery
 
     /// Returns the contents of a file with the given id.
     case fileContents(forFileId: String)
 
-    /// Returns a collection of unexpired announcements for the course with the id given.
-    case announcementsInCourse(withId: String)
+    /// Returns a folder along with its first-level children.
+    case folder(withId: String)
 
     /// Returns a collection of all events for a course.
     case eventsInCourse(withId: String)
@@ -33,42 +32,50 @@ public enum StudIpRoutes: ApiRoutes {
     /// Returns the profile picture at the URL given.
     case profilePicture(URL)
 
-    /// Returns the current user.
-    case currentUser
+    /// Returns the root folder along with its first-level children.
+    case rootFolderForCourse(withId: String)
+
+    /// Returns all semesters, usually starting about ten years in the past and ending one year in the
+    /// future.
+    case semesters
 
     var path: String {
         switch self {
-        case .semesters:
-            return "semesters"
-        case let .courses(userId):
-            return "user/\(userId)/courses"
-        case let .rootFolderForCourse(courseId):
-            return "course/\(courseId)/top_folder"
-        case let .folder(folderId):
-            return "folder/\(folderId)"
-        case let .fileContents(fileId):
-            return "file/\(fileId)/download"
         case let .announcementsInCourse(courseId):
             return "course/\(courseId)/news"
-        case let .eventsInCourse(courseId):
-            return "course/\(courseId)/events"
-        case let .profilePicture(url):
-            return url.path
         case .currentUser:
             return "user"
+        case let .courses(userId):
+            return "user/\(userId)/courses"
+        case .discovery:
+            return "discovery"
+        case let .eventsInCourse(courseId):
+            return "course/\(courseId)/events"
+        case let .fileContents(fileId):
+            return "file/\(fileId)/download"
+        case let .folder(folderId):
+            return "folder/\(folderId)"
+        case let .profilePicture(url):
+            return url.path
+        case let .rootFolderForCourse(courseId):
+            return "course/\(courseId)/top_folder"
+        case .semesters:
+            return "semesters"
         }
     }
 
     var type: Decodable.Type? {
         switch self {
-        case .semesters: return CollectionResponse<SemesterResponse>.self
-        case .courses: return CollectionResponse<CourseResponse>.self
-        case .rootFolderForCourse: return FolderResponse.self
-        case .folder: return FolderResponse.self
-        case .fileContents, .profilePicture: return nil
         case .announcementsInCourse: return CollectionResponse<AnnouncementResponse>.self
-        case .eventsInCourse: return CollectionResponse<EventResponse>.self
         case .currentUser: return UserResponse.self
+        case .courses: return CollectionResponse<CourseResponse>.self
+        case .discovery: return nil
+        case .eventsInCourse: return CollectionResponse<EventResponse>.self
+        case .fileContents: return nil
+        case .folder: return FolderResponse.self
+        case .profilePicture: return nil
+        case .rootFolderForCourse: return FolderResponse.self
+        case .semesters: return CollectionResponse<SemesterResponse>.self
         }
     }
 
@@ -76,7 +83,7 @@ public enum StudIpRoutes: ApiRoutes {
         switch self {
         case .fileContents:
             return 0
-        case .courses, .rootFolderForCourse, .folder, .announcementsInCourse, .currentUser:
+        case .discovery, .courses, .rootFolderForCourse, .folder, .announcementsInCourse, .currentUser:
             return 60
         case .semesters, .profilePicture, .eventsInCourse:
             return 60 * 60
