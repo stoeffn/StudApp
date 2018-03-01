3b8953e7a4e3d388abbfa2dfc6657db37a0c8567
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-03-01 14:09:31 +0100

    :lipstick: Add HTML content styles

--- StudApp/StudKitUI/HtmlContent/HtmlContentService.swift
+++ StudApp/StudKitUI/HtmlContent/HtmlContentService.swift
@@ -21,6 +21,8 @@ public final class HtmlContentService {
         return """
         <html>
             <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                 <style>
                     \(style)
                 </style>
