b3786f635a14ac8b6982956bbcec0a026e1387ed
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-04-05 11:20:29 +0200

    :page_facing_up: Apply copyright header to all source files

--- StudApp/StudKit/ViewModels/Colors/ColorPickerViewModel.swift
+++ StudApp/StudKit/ViewModels/Colors/ColorPickerViewModel.swift
@@ -1,9 +1,19 @@
 //
-//  ColorPickerViewModel.swift
-//  StudKit
+//  StudApp—Stud.IP to Go
+//  Copyright © 2018, Steffen Ryll
 //
-//  Created by Steffen Ryll on 11.09.17.
-//  Copyright © 2017 Steffen Ryll. All rights reserved.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 public final class ColorPickerViewModel<Color>: DataSourceSection {
