8107d26a65a6b57393067970fcd1fd8719601dd3
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-07-14 17:45:02 +0200

    :sparkles: Add blubber message type

---
+++ StudKit/StudIp/MessageResponse.swift
@@ -0,0 +1,45 @@
+//
+//  StudApp—Stud.IP to Go
+//  Copyright © 2018, Steffen Ryll
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import MessageKit
+
+struct Message: Decodable {
+    var blubber_id: String
+    var author: UserResponse
+    var content: String
+    var mkdate: String
+}
+
+extension Message: MessageType {
+    var sender: Sender {
+        return Sender(id: author.id, displayName: author.givenName + " " + author.familyName)
+    }
+
+    var messageId: String {
+        return blubber_id
+    }
+
+    var sentDate: Date {
+        guard let timeInterval = TimeInterval(mkdate) else { return .distantPast }
+        return Date(timeIntervalSince1970: timeInterval)
+    }
+
+    var kind: MessageKind {
+        return .text(content)
+    }
+}
