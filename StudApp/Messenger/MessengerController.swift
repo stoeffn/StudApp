869cf783f0a539f313c1d8e8e4639b45b475359b
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-07-14 17:28:05 +0200

    :poop: Implement example conversation view controller

---
+++ StudApp/Messenger/MessengerController.swift
@@ -0,0 +1,27 @@
+//
+//  StudApp—Stud.IP to Go
+//  Copyright © 2018, Steffen Ryll
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import StudKit
+
+final class MessengerController: UITableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = ConversationViewController()
+        controller.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(controller, animated: true)
+    }
+}
