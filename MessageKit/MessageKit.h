a82ac30b557565377fa661e7137b649133f5bf60
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-07-14 17:09:09 +0200

    :heavy_plus_sign: Add MessageKit and upgrade it to Swift 4.2

---
+++ MessageKit/MessageKit.h
@@ -0,0 +1,29 @@
+//
+//  StudApp—Stud.IP to Go
+//  Copyright © 2018, Steffen Ryll
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessageKit.
+FOUNDATION_EXPORT double MessageKitVersionNumber;
+
+//! Project version string for MessageKit.
+FOUNDATION_EXPORT const unsigned char MessageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageKit/PublicHeader.h>
+
+
