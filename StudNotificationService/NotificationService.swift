af11f2bff11c9b6c6783578f13c6c3e55cd5b841
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-07-14 14:30:47 +0200

    :tada: Add StudNotificationService

---
+++ StudNotificationService/NotificationService.swift
@@ -0,0 +1,38 @@
+//
+//  StudApp—Stud.IP to Go
+//  Copyright © 2018, Steffen Ryll
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UserNotifications
+
+final class NotificationService: UNNotificationServiceExtension {
+    private var contentHandler: ((UNNotificationContent) -> Void)?
+    private var content: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        content = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        guard let content = content else { return }
+        content.title = "\(content.title) [modified]"
+        contentHandler(content)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        guard let contentHandler = contentHandler, let content = content else { return }
+        contentHandler(content)
+    }
+}
